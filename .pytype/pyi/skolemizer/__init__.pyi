edce03d7cdec17e87c2335cf37bd414dba104bac
Stig B. Dørmænen <stigbd@gmail.com>
2021-05-19 12:00:05 +0200

    .gitignore is now working

--- .pytype/pyi/skolemizer/__init__.pyi
+++
@@ -1,9 +0,0 @@
-# (generated with --quick)
-
-import skolemizer.skolemizer
-from typing import Any, Type
-
-PackageNotFoundError: Any
-Skolemizer: Type[skolemizer.skolemizer.Skolemizer]
-__version__: Any
-version: Any
