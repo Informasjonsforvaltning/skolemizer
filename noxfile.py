2507bf251b7a0c2eb9c6ec5ead76970c6ee788a1
Nils Ove Tendenes <nils.ove@tendenes.com>
2026-04-01 09:34:15 +0200

    chore: update to python 3.12

--- noxfile.py
+++ noxfile.py
@@ -6,14 +6,13 @@ import nox
 import nox_poetry
 from nox_poetry import Session
 
-
 package = "skolemizer"
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
 nox.options.stop_on_first_error = True
 nox.options.sessions = "lint", "mypy", "tests"
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def tests(session: Session) -> None:
     """Run the test suite."""
     args = session.posargs or ["--cov"]
@@ -22,7 +21,7 @@ def tests(session: Session) -> None:
     session.run("pytest", *args)
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def black(session: Session) -> None:
     """Run black code formatter."""
     args = session.posargs or locations
@@ -30,7 +29,7 @@ def black(session: Session) -> None:
     session.run("black", *args)
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def lint(session: Session) -> None:
     """Lint using flake8."""
     args = session.posargs or locations
@@ -47,7 +46,7 @@ def lint(session: Session) -> None:
     session.run("flake8", *args)
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     with tempfile.NamedTemporaryFile() as requirements:
@@ -64,7 +63,7 @@ def safety(session: Session) -> None:
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or locations
@@ -72,7 +71,7 @@ def mypy(session: Session) -> None:
     session.run("mypy", *args)
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def xdoctest(session: Session) -> None:
     """Run examples with xdoctest."""
     args = session.posargs or ["all"]
@@ -81,7 +80,7 @@ def xdoctest(session: Session) -> None:
     session.run("python", "-m", "xdoctest", package, *args)
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def docs(session: Session) -> None:
     """Build the documentation."""
     session.run("poetry", "install", "--no-dev", external=True)
@@ -89,7 +88,7 @@ def docs(session: Session) -> None:
     session.run("sphinx-build", "docs", "docs/_build")
 
 
-@nox_poetry.session(python="3.10")
+@nox_poetry.session(python="3.12")
 def coverage(session: Session) -> None:
     """Upload coverage data."""
     session.install("coverage[toml]", "codecov")
